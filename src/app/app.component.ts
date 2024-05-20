de7199aa86af58d6f966e81326c6fea241ea8f81
Bryan-S10 <bryansmithp1010@gmail.com>
2024-05-20 17:44:31 -0500

    Hasta 4-agregar servicios, todo ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeroesComponent,HeroDetailComponent]
+    imports: [CommonModule, RouterOutlet, HeroesComponent,HeroDetailComponent,MessagesComponent]
 })
 export class AppComponent {
   title = 'Machines Tour';
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
-
+import { HeroService } from '../hero.service';
 import { FormsModule } from '@angular/forms';
-import { HEROES } from "../mock-heroes";
+
 import { NgFor } from '@angular/common';
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import { MessageService } from '../message.service';
 @Component({
     selector: 'app-heroes',
     standalone: true,
@@ -16,18 +17,25 @@ import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 export class HeroesComponent implements OnInit {
 
   hero!: Hero;
-  heroes = HEROES;
+  heroes : Hero[] | undefined;
   selectedHero!: Hero;
 
-  constructor() {
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {
 
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
 
     // window.alert(`Selected Hero: ${hero.name}`);
     console.log("asdasdas");
